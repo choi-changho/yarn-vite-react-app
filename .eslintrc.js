4864a174807e8be07a966fbd4d4fa741a79b7baf
choi-chang-ho <choich@healthhub.kr>
2023-03-15 11:21:01 +0900

    Eslint, prettier 설정

---
+++ .eslintrc.js
@@ -0,0 +1,77 @@
+module.exports = {
+	parser: '@typescript-eslint/parser',
+	parserOptions: {
+		ecmaFeatures: {
+			jsx: true,
+		},
+		ecmaVersion: 13,
+		sourceType: 'module',
+	},
+	plugins: ['react', '@typescript-eslint'],
+	extends: [
+		'eslint:recommended',
+		'plugin:@typescript-eslint/recommended',
+		'plugin:react/recommended',
+		'airbnb',
+		'plugin:prettier/recommended',
+	],
+	env: {
+		browser: true,
+		es2021: true,
+		node: true,
+	},
+	settings: {
+		react: {
+			version: 'detect',
+		},
+		'import/parsers': {
+			'@typescript-eslint/parser': ['.ts', '.tsx'],
+		},
+		'import/resolver': {
+			typescript: {},
+		},
+	},
+	rules: {
+		// 0 false, 1 warn, 2 error
+		'import/no-extraneous-dependencies': ['error', {devDependencies: true}], // Dev Dependency lint 에러 끄기
+		'import/extensions': [
+			'error',
+			'ignorePackages',
+			{
+				js: 'never',
+				jsx: 'never',
+				ts: 'never',
+				tsx: 'never',
+				json: 'never',
+			},
+		], // import 시 extensions 체크 에러 끄기
+		'import/prefer-default-export': 0, // 하나의 파일에서 단일로 export 하는 경우에 대한 경고 끄기
+		'prettier/prettier': 0, // prettier 에 대한 경고 끄기
+		'for-direction': 2, // for 루프가 무한대로 실행되는 것을 방지
+		'no-undef': 2,
+		'no-console': 2,
+		'no-empty': 2,
+		'no-dupe-args': 2,
+		'no-dupe-keys': 2,
+		'no-unreachable': 2,
+		'react/jsx-indent': [2, 'tab'], // jsx에서도 indent tab으로 통일
+		'react/jsx-key': 2,
+		'react/jsx-no-duplicate-props': 2,
+		'react/jsx-no-undef': 2,
+		'react/jsx-uses-vars': 2,
+		'react/no-children-prop': 2,
+		'react/no-deprecated': 2, // react version 감지하여 deprecated 된 함수 경고
+		'react/no-direct-mutation-state': 2, // state 를 직접 바꾸지 않게하기 위한 옵션
+		'react/no-is-mounted': 2,
+		'react/no-render-return-value': 2,
+		'react/no-unknown-property': 2,
+		'react/require-render-return': 2, // render method를 작성할때 return 이 없으면 경고
+		'react/jsx-filename-extension': [1, {extensions: ['.tsx']}], // jsx에서 tsx 파일 쓸려고 할때 에러
+		'react/react-in-jsx-scope': 0, // vite react plugin에서 자동으로 주입하기 때문에 제거
+		'react/require-default-props': 0, // component default prop 지정에 대한 경고 제거
+		'react/jsx-props-no-spreading': 1, // props로 받은 것 바로 props로 넘기기 허용
+		'react/function-component-definition': 0, // react 함수형 컴포넌트 정의 방식 선언
+		'react/no-unescaped-entities': 0, // react jsx 내 따옴표 escape 여부
+		// 'react/prop-types': 1,
+	},
+};
--- README.md
+++ README.md
@@ -13,11 +13,20 @@ yarn create vite [프로젝트 명] --template react-ts
 ```shell script
 2. 스토리북 설치(command)
 npx sb init --builder @storybook/builder-vite
--- storybook init - the simplest way to add a Storybook to your project. 
+* storybook init - the simplest way to add a Storybook to your project. 
 위의 문장이 나왔을때 init (command)
 >> .storybook 폴더 생성 및 package.json dependency 및 스크립트 추가
 ```
 ```shell script
 3. Emotion 설치(command)
 yarn add @emotion/styled @emotion/react emotion-reset
+```
+```shell script
+4. Eslint / Prettier 설치
+yarn add -D eslint eslint-config-airbnb eslint-config-prettier eslint-plugin-import eslint-plugin-jsx-a11y eslint-plugin-prettier eslint-plugin-react eslint-plugin-react-hooks
+* Typescript eslint 설치
+yarn add -D @typescript-eslint/eslint-plugin @typescript-eslint/parser
+* 설정 및 인텔리제이 세팅 참고 자료
+- https://minjung-jeon.github.io/eslint-prettier-intellij/
+- https://min33sky.github.io/posts/paths-tsconfig-setting/
 ```
